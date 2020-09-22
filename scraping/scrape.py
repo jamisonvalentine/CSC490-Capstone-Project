c8d0b764b27e1a71f4be9d0fa5fc83b62c365cf2
Jamison Valentine <jamisont.valentine@gmail.com>
2020-09-22 17:45:07 -0400

    update list of classes and work on scraper

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -4,6 +4,7 @@
 
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 import time
 
 PATH = "../selenium/chromedriver.exe"
@@ -19,8 +20,9 @@ driver = webdriver.Chrome(PATH)
 # General process
 # 1) go to site
 # 2) click "Advanced Search" tab
-# 3) click "Location" dropdown/select
-# 4) Click "Internet Based /';Course" option
+# 3) click "Term" dropdown/select
+# 4) click "Location" dropdown/select
+# 5) Click "Internet Based /';Course" option
 #	for each page:
 #		for each course:
 
@@ -37,44 +39,91 @@ driver = webdriver.Chrome(PATH)
 # 2) click "Advanced Search" tab
 # 3) click "Location" dropdown/select
 # 4) Click "Internet Based Course" option
-#
+
 
 
 # interface either self service or webadvisor
 # pass 0 for self service
 # pass 1 for webadvisor
-def get_school_data(interface, url):
+def get_school_data(url, abbr):
     driver.get(url)
-    time.sleep(3)
-    print(driver.title)
+    time.sleep(1)
+
     tab = driver.find_elements_by_class_name("esg-tab__link")[1]
     tab.click()
     time.sleep(1)
-    selects = driver.find_elements_by_id("term-label-id")
 
-    for index, val in enumerate(selects):
-        print('# ' + str(index) + ": " + val.get_attribute("id"))
+    term = Select(driver.find_elements_by_id("term-label-id")[1])
+    term.select_by_value("2020FA")
 
-        selects[1].click()
+    location = driver.find_element_by_css_selector("option[value='OL']")
+    location.click()
+
+    search_btn = driver.find_element_by_css_selector("input[value='Search']")
+    search_btn.click()
+    time.sleep(4)
+
+    #####################
+    # Extract Course and Section information from pages
+    ######################
+    last = get_total_pages() - 1
+    for i in range(last):
+
+        # click all buttons on page
+        courselist = driver.find_element_by_id("course-resultul")
+        click_course_bnts(courselist)
 
 
-# # links = driver.find_elements_by_tag_name("a")
-# # for index, val in enumerate(links):
-# #     print('# ' + str(index) + ": " + val.get_attribute("class"))
+        # wait
+        time.sleep(10)
+        with open("output/ss/" + abbr + "/source" + page_num + ".html", "w") as file:
+            file.write(driver.page_source)
 
-# lists = driver.find_elements_by_tag_name("li")
-# for index, val in enumerate(lists):
-#     print('# ' + str(index) + ": " + val.get_attribute("class"))
+        # parse page
+        #esg-section--margin-top esg-section--border
+        courses = courselist.find_elements_by_tag_name("li")
 
-# html = driver.page_source
-# soup = bs(html, 'lxml')
-# link = soup.findAll("a")
-# print(len(link))
+        # for each course
+        for course in courses:
+            sections = course.find_elements_by_css_selector('ul[data-bind="foreach: Sections"]')
 
-# tab = driver.find_elements_by_class_name("esg-tab__link")
-# print(tab)
-# print(driver.page_source)
-# driver.quit()
-get_school_data(0, "https://sss.halifaxcc.edu/Student/Courses")
+        # for each section
+
+        # write to file
+
+
+        # if not last page -> next page
+        if i != last:
+            next_page()
+
+
+
+get_school_data("https://sss.halifaxcc.edu/Student/Courses")
 # get_school_data(
 #     1, "https://online.blueridge.edu/WA/WebAdvisor?TOKENIDX=4390841124&SS=1&APP=ST&CONSTITUENCY=WBST")
+
+
+# courses = <ul id=course-resultul" ... >
+# -> course = <li>
+# -> -> sections = <ul data-bind="foreach: Sections">
+# -> -> -> section = <li class="search-nestedaccordionitem">
+# -> -> -> ->
+
+
+# Total pages
+# total_pages = driver.find_element_by_css_selector("span[data-bind='text:TotalPages']").
+
+
+def get_total_pages():
+    return driver.find_element_by_css_selector("span[data-bind='text:TotalPages']").text
+
+def next_page():
+    next_page_btn = driver.find_element_by_id("course-results-next-page")
+    next_page_btn.click()
+
+def click_course_bnts(_courselist):
+    collapsed = _courselist.find_elements_by_css_selector('button[class="esg-collapsible-group__toggle text-align-left"]')
+    print("# of courses: ", len(collapsed))
+    for button in collapsed:
+        button.click()
+    
