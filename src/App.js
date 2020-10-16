2cd3b3845a05cd6e3f6999e02a3dc02c26c9d57d
raiana zaman <raiana.salmin@gmail.com>
2020-10-16 15:11:12 -0400

    UX/UI updates

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Home from './components/Home';
+import {
+  Switch,
+  Route,
+  match
+} from "react-router-dom";
+import About from './components/About';
+import Error from './components/404';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <div className='app d-flex flex-column justify-content-between'>
+      <Switch>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/about">
+          <About/>
+        </Route>
+
+        
+        <Route path='/search'>
+          <Search/>
+        </Route>
+        
+        <Error/>
+
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/404.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './lib/Header';
+import Footer from './lib/Footer';
+
+const Error = () => {
+
+    return (
+        <>
+            <div className="d-flex justify-content-center align-items-center h-100">
+                <h2 className="text-dark text-center font-weight-bold">404</h2>
+            </div>
+            
+        </>
+    );
+}
+
+export default Error;
