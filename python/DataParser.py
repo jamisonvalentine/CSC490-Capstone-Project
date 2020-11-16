b39495dacfb30219c664876610ac3c64a8afcbdc
JeremyHudson43 <jer4349@gmail.com>
2020-11-16 08:43:02 -0500

    updated parser and course listings

--- python/DataParser.py
+++ python/DataParser.py
@@ -33,15 +33,19 @@ def determineEquivalencies(iterator):
 
     fullComColCourse = globalSubject[iterator] + "-" + globalClassID[iterator]
 
+    # check if course exists in equivalencies
     if comColID.count(fullComColCourse) > 0:
 
+        # get the index of the equivalency
         equivalentIndex = comColID.index(fullComColCourse)
 
+        # add UNCG title and ID at that index
         globalUncgID.append(uncgID[equivalentIndex])
         globalUncgTitle.append(uncgTitle[equivalentIndex])
+
     else:
-            globalUncgID.append("No equivalency found")
-            globalUncgTitle.append("No equivalency found")
+        globalUncgID.append("No equivalency found")
+        globalUncgTitle.append("No equivalency found")
 
 
 def printResults(iterator):
@@ -67,7 +71,7 @@ def printResults(iterator):
 
 
 def standardizeOnline(iterator):
-    location = str(df.iloc[iterator, 6])
+    location = str(df.iloc[iterator, 5])
 
     standardOnline = "Online"
 
@@ -148,7 +152,7 @@ def addWebsite(iterator):
 
 
 def extractSubIDandNum(iterator):
-    section = df.iloc[iterator, 2]
+    section = df.iloc[iterator, 1]
 
     # split section on -
     splitSection = str(section).split("-")
@@ -174,7 +178,7 @@ def extractSubIDandNum(iterator):
 
 
 def extractCourseName(iterator):
-    section = df.iloc[iterator, 2]
+    section = df.iloc[iterator, 1]
 
     nameOfCourse = ""
 
@@ -188,45 +192,59 @@ def extractCourseName(iterator):
 
 
 def extractTime(iterator):
-    dates = df.iloc[iterator, 5]
+    dates = df.iloc[iterator, 4]
 
     # find time by locating : value and getting left and right sides
     time = dates[dates.find(':') - 2: dates.find(':') + 15]
 
+    firstTime = time.split("-")[0][:-3]
+
+    # ensure time matches proper format
+    time_re = re.compile(r'^(([01]\d|2[0-3]):([0-5]\d)|24:00)$')
+    properTime = bool(time_re.match(firstTime))
+
     if time.find(':') == -1:
+        time = "No time given"
+    elif not properTime:
         time = "No time given"
 
     globalTimeOfCourse.append(time)
 
 
+
 def extractBuilding(iterator):
-    dates = df.iloc[iterator, 5]
+    dates = df.iloc[iterator, 4]
 
-    daysToFilter = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    try:
+        daysToFilter = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-    # remove days from full date string so comma split below will work properly
-    for day in daysToFilter:
-        dates = dates.replace(day + ",", "")
+        # remove days from full date string so comma split below will work properly
+        for day in daysToFilter:
+            dates = dates.replace(day + ",", "")
 
-    if "ONLNE" in dates or "Distance" in dates:
-        building = "Online"
-    elif "Times to be Announced" in dates:
-        building = "TBA"
-    else:
-        # select right side of split on comma after filtering out days
-        building = dates.split(",")[1]
+        if "ONLNE" in dates or "Distance" in dates:
+            building = "Online"
+        elif "Times to be Announced" in dates:
+            building = "TBA"
+        else:
+            # select right side of split on comma after filtering out days
+            building = dates.split(",")[1]
 
-        # remove excess trailing information for a few cases
-        building = ",".join(building.split(",", 2)[:2])
+            # remove excess trailing information for a few cases
+            building = ",".join(building.split(",", 2)[:2])
 
-    if (not building):
-        building = "No building found"
+        if (not building):
+            building = "No building found"
 
-    globalCourseBuilding.append(building)
+        globalCourseBuilding.append(building)
+
+    except:
+      globalCourseBuilding.append("No building found")
+
 
 
 def extractDays(iterator):
-    dates = df.iloc[iterator, 5]
+    dates = df.iloc[iterator, 4]
     days = ""
 
     acceptableDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
@@ -247,7 +265,7 @@ def extractDays(iterator):
 
 
 def extractDateRange(iterator):
-    dates = df.iloc[iterator, 5]
+    dates = df.iloc[iterator, 4]
 
     # extract left side after split on space to get date range
     dateRange = dates.split()[0].strip()
@@ -256,25 +274,29 @@ def extractDateRange(iterator):
 
 
 def extractRoom(iterator):
-    dates = df.iloc[iterator, 5]
+    try:
+        dates = df.iloc[iterator, 4]
 
-    # split right on Room to get everything after
-    room = dates.rsplit("Room")[1]
+        # split right on Room to get everything after
+        room = dates.rsplit("Room")[1]
 
-    # split on parenthesis to remove trailing info after room
-    room = room.split("(")[0]
+        # split on parenthesis to remove trailing info after room
+        room = room.split("(")[0]
 
-    if "ONLNE" in dates or "ONLINE" in dates:
-        room = "Online"
+        if "ONLNE" in dates or "ONLINE" in dates:
+            room = "Online"
 
-    if "Room to be Announced" in dates:
-        room = "TBA"
+        if "Room to be Announced" in dates:
+            room = "TBA"
 
-    globalRoom.append(room)
+        globalRoom.append(room)
+
+    except:
+         globalRoom.append("No room found")
 
 
 def extractSemesterLength(iterator):
-    dates = df.iloc[iterator, 5]
+    dates = df.iloc[iterator, 4]
 
     # extract left side after split on space to get date range
     dateRange = dates.split()[0].strip()
@@ -307,11 +329,11 @@ def calculateCost(iterator):
 
 
 def extractSemester():
-    globalSemester.append("Fall")
+    globalSemester.append("Spring")
 
 
 def extractYear():
-    globalYear.append("2020")
+    globalYear.append("2021")
 
 
 def splitSection(iterator):
@@ -339,7 +361,7 @@ def splitDates(iterator):
 
 if __name__ == '__main__':
 
-    df = pd.read_csv("wa_courses.csv")
+    df = pd.read_csv("wa_courses_sp2021.csv")
 
     collegeName = df["college"].to_list()
     collegeCredits = df["credits"].to_list()
